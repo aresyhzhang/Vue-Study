5c433a0d770d9972301373c560b916351b492989
n21614 <1534280186@qq.com>
2022-01-21 11:32:41 +0800

    20220121

---
+++ project/app/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+<div>
+     <Header></Header>
+    <Footer></Footer>
+</div>
+ 
+</template>
+
+<script>
+import Header from './components/header'
+import Footer from './components/footer'
+
+export default {
+  name: 'App',
+  components: {
+    Header,
+    Footer
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
